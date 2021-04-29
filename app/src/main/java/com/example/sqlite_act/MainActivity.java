c4e94e2d9d2b3ecd20e12e5690ecff4584602b92
dnn <dnatayawibisono@gmail.com>
2021-04-29 22:38:26 +0700

    teman baru layout

--- app/src/main/java/com/example/sqlite_act/MainActivity.java
+++ app/src/main/java/com/example/sqlite_act/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sqlite_act.adapter.TemanAdapter;
 import com.example.sqlite_act.database.DBController;
 import com.example.sqlite_act.database.Teman;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Teman> temanArrayList;
     DBController controller = new DBController(this);
     String id, nm, tlp;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+        fab = findViewById(R.id.floatingBtn);
         BacaData();
         adapter = new TemanAdapter(temanArrayList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TemanBaru.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void BacaData() {
---
+++ app/src/main/java/com/example/sqlite_act/TemanBaru.java
@@ -0,0 +1,14 @@
+package com.example.sqlite_act;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TemanBaru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman_baru);
+    }
+}
